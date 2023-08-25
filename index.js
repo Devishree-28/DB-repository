2dd5ff82e097d7e45b370d58862c8a8daa70590d
Devishree-28 <142956573+Devishree-28@users.noreply.github.com>
2023-08-25 21:30:11 +0530

    star_rating_js

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 // DON'T remove the below import
 import "./styles.css";
-let quote = document.getElementById("quote");
-    let author = document.getElementById("author");
-    let btn = document.getElementById("btn");
+    const allstar = document.querySelectorAll(".star");
+        let current_star_level = document.querySelector('.current_rating');
 
-    const url = "https://api.quotable.io/random";
+        allstar.forEach((star, i) => {
+            star.onclick = function() {
+                let current_star_level_value = i + 1;
+                //current_star_level.innerText = `${current_star_level_value} of 5`;
+                if(current_star_level_value == 1){
+                    current_star_level.innerText = "I just hate it";
+                }
+                else if(current_star_level_value == 2)
+                {
+                    current_star_level.innerText = "I don't like it";
+                }
+                else if(current_star_level_value == 3)
+                {
+                     current_star_level.innerText = "It is awesome";
+                }
+                else if(current_star_level_value == 4)
+                {
+                     current_star_level.innerText = "I just like it";
+                } else 
+                {
+                    current_star_level.innerText = "Excellent! We are thrilled to hear you had such a positive experience, Thank you for choosing our platform.";
+                }
 
-    let getQuote = () => {
-      fetch(url)
-        .then((data) => data.json())
-        .then((item) => {
-          quote.innerText = item.content;
-          author.innerText = item.author;
-        });
-    };
-
-    window.addEventListener("load", getQuote);
-    btn.addEventListener("click", getQuote);
+                allstar.forEach((star, j) => {
+                    if( current_star_level_value >= j+1 )
+                    {
+                        star.innerHTML = '&#9733';
+                    }else{
+                        star.innerHTML = '&#9734';
+                    }
+                })
+            }
+        })
